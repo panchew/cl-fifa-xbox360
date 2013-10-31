02f67c996d2de5d9fb5b613ecbbef775d5ff0eb3
Francisco Guzmán <jf.guzman76@gmail.com>
2013-10-30 22:31:52 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClFifaXbox360::Application.config.secret_token = '057add45cf90f478489c6d28e6af16775279b95f221c9ff85898ac1318801d6c94dc2f32c3991ac9c8c69c5ac046eeaad897453fe17848b756a06d1783036684'
